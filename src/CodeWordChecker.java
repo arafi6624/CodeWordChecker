4fd7fdc1b2ca6b1ebfa6e30996e4d4c83bbf0601
BT_1N3_04@BKTECH.LOCAL <BT_1N3_04@BKTECH.LOCAL>
2018-11-19 14:47:09 -0500

    Initial commit

---
+++ src/CodeWordChecker.java
@@ -0,0 +1,25 @@
+public class CodeWordChecker implements StringChecker{
+    private String word;
+    private int min;
+    private int max;
+
+    public CodeWordChecker (int min, int max, String word){
+        this.min = min;
+        this.max = max;
+        this.word = word;
+    }
+
+    public CodeWordChecker (String word){
+        this.min = 6;
+        this.max = 20;
+        this.word = word;
+    }
+
+    public boolean isValid(String input){
+        if (input.length() >= min || input.length() <= max){
+            if(input.indexOf(word) = -1){
+                return true;
+            }
+        }
+    }
+}
