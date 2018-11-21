c92ad2f140156d9b89acfee4efebdfba0d7e6968
BT_1N3_04 <arafi6624@bths.edu>
2018-11-21 14:29:51 -0500

    final

--- src/CodeWordChecker.java
+++ src/CodeWordChecker.java
@@ -3,13 +3,13 @@ public class CodeWordChecker implements StringChecker{
     private int min;
     private int max;
 
-    public CodeWordChecker (int min, int max, String word){
+    public CodeWordChecker(int min, int max, String word){
         this.min = min;
         this.max = max;
         this.word = word;
     }
 
-    public CodeWordChecker (String word){
+    public CodeWordChecker(String word){
         this.min = 6;
         this.max = 20;
         this.word = word;
@@ -17,12 +17,10 @@ public class CodeWordChecker implements StringChecker{
 
     public boolean isValid(String input){
         if (input.length() >= min && input.length() <= max){
-            if(input.indexOf(word) = -1){
+            if(input.indexOf(word) == -1){
                 return true;
             }
         }
-        else {
-            return false;
-        }
+        return false;
     }
 }
